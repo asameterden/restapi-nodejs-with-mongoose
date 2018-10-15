add0909a2516975c32283aa47259a9779072ea72
asameterden <asameterden@gmail.com>
2018-10-15 23:26:26 +0300

    create order's method

--- api/routes/orders.js
+++ api/routes/orders.js
@@ -1,43 +1,91 @@
 const express = require('express');
 const router = express.Router();
+const mongoose = require('mongoose');
+const Order =  require('../models/order');
+const Product = require('../models/product');
 
 //list
 router.get('/',(req,res,next)=>{
-    res.status(200).json({
-        message : "Handling get requests to /orders"
-    });
+    Order.find()
+    .select('product quantity _id')
+    .exec()
+    .then(docs =>{
+        res.status(200).json({
+            count : docs.length,
+            orders : docs.map(doc => {
+                return {
+                    _id : doc._id,
+                    product : doc.product,
+                    quantity : doc.quantity , 
+                    request : {
+                        type : 'GET',
+                        url : 'http://localhost:3000/orders/' + doc._id
+                    }
+                }
+            })
+        });
+    })
+    .catch(err=>{
+        res.status(500).json({error : err});
+    })
 });
 
 //show
 router.get('/:orderId',(req,res,next)=>{
-    res.status(200).json({
-        message : "Handling get requests to /orders/id"
+    Order.findById({_id : req.params.orderId})
+    .exec()
+    .then(order =>{
+        res.status(200).json({order : order});
+    })
+    .catch(err => {
+        res.status(500).json({error : err});
     });
 });
 
 //create
 router.post('/',(req,res,next)=>{
-    const order = {
-        productId:req.body.productId,
-        quantity : req.body.quantity
-    }
-    res.status(200).json({
-        message : "Order was Created",
-        order : order
+    Product.findById(req.body.productId)
+    .then(product => {
+        if(!product){
+            res.status(404).json({
+                message : "Product not found"
+            });
+        }
+        const order = new Order({
+            _id : mongoose.Types.ObjectId(),
+            quantity : req.body.quantity,
+            product : req.body.productId,
+        });
+       return order.save()
+    })
+    .then(result =>{
+        res.status(201).json({
+            message:'Order stored',
+            createdOrders : {
+                _id : result._id,
+                product : result.product,
+                quantity : result.quantity,
+                request :{
+                    type : 'GET',
+                    url : 'http://localhost:3000/orders/' + result._id
+
+                }
+            }
+        });
+    })
+    .catch(err=>{
+        res.status(500).json({error : err});
     });
 });
 
-//update
-router.patch('/:orderId',(req,res,next)=>{
-    res.status(200).json({
-        message : "Handling get requests to orders update"
-    });
-});
 
 //delete
 router.delete('/:ordertId',(req,res,next)=>{
-    res.status(200).json({
-        message : "Handling get requests to products delete"
+    Order.remove({_id :req.params.orderId})
+    .exec()
+    .then(res.status(200).json(result))
+    .catch(err => {
+        res.status(500).json({error : err});
     });
 });
 
