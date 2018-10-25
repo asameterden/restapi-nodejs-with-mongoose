aad2b9613c4871d22615b30e7fd918fdd4557a5c
asameterden <asameterden@gmail.com>
2018-10-25 14:03:47 +0300

    sing up

--- app.js
+++ app.js
@@ -6,6 +6,7 @@ const mongoose = require('mongoose');
 
 const productRoutes = require('./api/routes/products');
 const orderRoutes = require('./api/routes/orders');
+const userRoutes = require('./api/routes/user');
 
 mongoose.Promise = global.Promise;
 mongoose.connect("mongodb://asameterden:"+process.env.MONGO_ATLAS_PW+"@cluster0-shard-00-00-dhsmj.mongodb.net:27017,cluster0-shard-00-01-dhsmj.mongodb.net:27017,cluster0-shard-00-02-dhsmj.mongodb.net:27017/test?ssl=true&replicaSet=Cluster0-shard-0&authSource=admin&retryWrites=true",{
@@ -45,6 +46,7 @@ app.use((req,res,next) => {
 //Routes which shoul handle requests
 app.use('/products',productRoutes);
 app.use('/orders',orderRoutes);
+app.use('/users',userRoutes);
 
 app.use((req,res,next)=>{
     const error = new Error('Not Found');
